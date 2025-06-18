2234ea15b46032b91092d71885b19da76044581c
mariano-git <caminosmariano1@gmail.com>
2025-06-17 23:56:35 -0300

    adding the register vw-ctl

--- Core/Validator.php
+++ Core/Validator.php
@@ -5,6 +5,11 @@ namespace Core;
 class Validator
 {
 
+    public static function email($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
     // funcion pura, no depende de estados o valores del exterior
     public static function string($value, $min = 1, $max = INF)
     {
---
+++ controllers/registration/store.php
@@ -0,0 +1,64 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// validate the user
+
+if(!Validator::email($email))
+{
+    $errors['email'] = "Please enter a valid email address.";
+}
+
+if(!Validator::string($password, 7, 255))
+{
+    $errors['password'] = "Please enter a password between 7 and 255 characters.";
+}
+
+if(!empty($errors))
+{
+    return view("registration/create.view.php", [
+        "errors" => $errors
+    ]);
+}
+
+$db = App::resolve(Database::class);
+
+// Check if the account already exists
+
+$user = $db->Query("SELECT * FROM users WHERE email = :email", [
+    'email' => $email
+])->find();
+
+if($user)
+{
+    // if someone with that email already exists and has an account redirect him to a login page.
+    $_SESSION["user"] = [
+        "email" => $email
+    ];
+
+    header("location: /");
+    exit();
+}
+else {
+
+    // if not, save one to the database and then log the user in, and redirect.
+    $db->Query("INSERT INTO users(email, password) 
+    VALUES (:email, :password)"
+    , [
+        "email" => $email,
+        "password" => $password
+    ]);
+
+    // mark that the user has logged in.
+    $_SESSION["user"] = [
+        "email" => $email
+    ];
+
+    header("location: /");
+    exit();
+}
